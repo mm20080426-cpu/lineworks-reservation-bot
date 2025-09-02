dca374e2c4a59816fa1cadb40282fda0e4d8a33f
mm20080426-cpu <mm20080426@gmail.com>
2025-09-02 16:20:08 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+// index.js
+const fetchAccessToken = require('./tokenFetcher');
+
+fetchAccessToken();
---
+++ jwtGenerator.js
@@ -0,0 +1,23 @@
+// jwtGenerator.js
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
+
+function generateJWT() {
+  const now = Math.floor(Date.now() / 1000);
+  const payload = {
+    iss: process.env.SERVICE_ACCOUNT,
+    sub: process.env.SERVICE_ACCOUNT,
+    iat: now,
+    exp: now + 3600
+  };
+
+  return jwt.sign(payload, PRIVATE_KEY, {
+    algorithm: 'RS256',
+    header: { alg: 'RS256', typ: 'JWT' }
+  });
+}
+
+module.exports = generateJWT;
