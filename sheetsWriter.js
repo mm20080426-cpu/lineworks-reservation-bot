d738a552ddb153e162abfab3ae911a688e59e27e
mm20080426-cpu <mm20080426@gmail.com>
2025-09-06 22:52:23 +0900

    Add files via upload

---
+++ sheetsWriter.js
@@ -0,0 +1,25 @@
+const { google } = require('googleapis');
+const path = require('path');
+
+async function writeReservationData(dataArray) {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: path.join(__dirname, 'google-credentials.json'), // ← ファイルから読み込む
+    scopes: ['https://www.googleapis.com/auth/spreadsheets']
+  });
+
+  const sheets = google.sheets({ version: 'v4', auth });
+
+  const spreadsheetId = process.env.GS_SPREADSHEET_ID;
+  const sheetName = process.env.GS_SHEET_NAME;
+
+  await sheets.spreadsheets.values.append({
+    spreadsheetId,
+    range: `${sheetName}!A1`,
+    valueInputOption: 'USER_ENTERED',
+    requestBody: {
+      values: dataArray
+    }
+  });
+}
+
+module.exports = { writeReservationData };
